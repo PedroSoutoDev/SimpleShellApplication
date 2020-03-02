2e54d920fdc96c6cd4b9b07f53ebbe8e9e9269df
Pedro Souto <Pedro.Souto.SFSU@gmail.com>
2020-03-02 15:39:12 -0800

    Initial setup

---
+++ SimpleShell/SimpleShell/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SimpleShell
+//
+//  Created by Pedro Souto on 3/2/20.
+//  Copyright © 2020 Pedro Souto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
